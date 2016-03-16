4259e2869874e9680e9cf86c5e27d560a8c74842
Rich Matthews <richmatthews@notonthehighstreet.com>
2016-03-16 20:50:25 +0000

    WIP

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 import random
 
-score = 0
 print('Exam started')
 
-def questions():
-    global score
+class allTheQuestions:
+    answers = ['def', 'break', 'len(string)']
     q1options = ['func', 'def', 'function', 'define'];
-    q1answer = q1options[1];
-    print(q1options)
-    q1 = raw_input('What is the keyword for functions in Python? ');
-    if (q1 == q1answer):
-        score = score + 1
-def q2():
-    global score
     q2options = ['break', 'loop out', 'stop', 'abandon'];
-    q2answer = q2options[0];
-    print(q2options)
-    q2 = raw_input('What keyword terminates a loop in Python? ');
-    if (q2 == q2answer):
-        score = score + 1
-def q3():
-    global score
     q3options = ['string.length', 'str(length)', 'len(string)', 'len.string'];
-    q3answer = q3options[2];
-    print(q3options)
-    q3 = raw_input('How do you get the length of a string? ');
-    if (q3 == q3answer):
-        score = score + 1
 
-questionList = [q1(), q2(), q3()]
-h = [1, 2, 3, 4 , 5]
-random.shuffle(questionList)
-random.shuffle(h)
-print h
+
+class shufflingQuestions:
+    def shuffling():
+        '''sq = shufflingQuestions.shuffling()
+        questionList = [q1, q2, q3]
+        random.shuffle(questionList)
+        questionShuffled = questionList
+        questionList.pop(0)
+        return questionShuffled'''
+
+    def questions():
+        ques1 = raw_input('What is the keyword for functions in Python? ');
+        ques2 = raw_input('What keyword terminates a loop in Python? ');
+        ques3 = raw_input('How do you get the length of a string? ');
+        score = 0
+
+        questionList = [ques1, ques2, ques3]
+        while (len(questionList) > 0):
+            for q in questionList:
+                random.shuffle(questionList)
+                questionShuffled = questionList
+                print(questionShuffled[0])
+                questionShuffled.pop(0)
+    questions()
+'''global q1answer, q2answer, q3answer
+if q1answer == answers[0]:
+    score += score + 1
+if q2answer == answers[1]:
+    score += score + 1
+if q3answer == answers[2]:
+    score += score + 1
+'''
