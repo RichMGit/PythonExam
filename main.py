3d88dbf509f8d3cabf675d9804f977e86f15c0a6
Rich Matthews <richmatthews@notonthehighstreet.com>
2016-03-17 17:27:48 +0000

    WIP

--- main.py
+++ main.py
@@ -3,40 +3,28 @@ import random
 print('Exam started')
 
 class allTheQuestions:
-    answers = ['def', 'break', 'len(string)']
-    q1options = ['func', 'def', 'function', 'define'];
-    q2options = ['break', 'loop out', 'stop', 'abandon'];
-    q3options = ['string.length', 'str(length)', 'len(string)', 'len.string'];
 
+    q1options = ['func', 'def', 'function', 'define']
+    q2options = ['break', 'loop out', 'stop', 'abandon']
+    q3options = ['string.length', 'str(length)', 'len(string)', 'len.string']
 
 class shufflingQuestions:
-    def shuffling():
-        '''sq = shufflingQuestions.shuffling()
-        questionList = [q1, q2, q3]
-        random.shuffle(questionList)
-        questionShuffled = questionList
-        questionList.pop(0)
-        return questionShuffled'''
 
     def questions():
-        ques1 = raw_input('What is the keyword for functions in Python? ');
-        ques2 = raw_input('What keyword terminates a loop in Python? ');
-        ques3 = raw_input('How do you get the length of a string? ');
-        score = 0
+        ques1 = 'What is the keyword for functions in Python? '
+        ques2 = 'What keyword terminates a loop in Python? '
+        ques3 = 'How do you get the length of a string? '
 
-        questionList = [ques1, ques2, ques3]
-        while (len(questionList) > 0):
-            for q in questionList:
-                random.shuffle(questionList)
-                questionShuffled = questionList
-                print(questionShuffled[0])
-                questionShuffled.pop(0)
+        score = 0
+        answer = 0
+        quesAndAns = [[ques1, 'def'], [ques2, 'break'], [ques3, 'len(string)']]
+        while (len(quesAndAns) > 0):
+            for q in quesAndAns:
+                random.shuffle(quesAndAns)
+                print (quesAndAns)
+                '''questionShuffled = quesAndAns
+                a = raw_input (questionShuffled[0])
+                quesAndAns.append(a)
+                questionShuffled.pop(0)'''
+        print (allArray[0][1])
     questions()
-'''global q1answer, q2answer, q3answer
-if q1answer == answers[0]:
-    score += score + 1
-if q2answer == answers[1]:
-    score += score + 1
-if q3answer == answers[2]:
-    score += score + 1
-'''
