c9d81edbfd637c56f6130dcd9e5019abd9a17548
Rich Matthews <richmatthews@notonthehighstreet.com>
2016-03-21 13:42:30 +0000

    WIP

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 import random
+from HTMLParser import HTMLParser
 
 print('Exam started')
 
-class allTheQuestions:
-
-    q1options = ['func', 'def', 'function', 'define']
-    q2options = ['break', 'loop out', 'stop', 'abandon']
-    q3options = ['string.length', 'str(length)', 'len(string)', 'len.string']
-
 class shufflingQuestions:
-
+    def main():
+        parser = HTMLparser()
     def questions():
-        ques1 = 'What is the keyword for functions in Python? '
-        ques2 = 'What keyword terminates a loop in Python? '
-        ques3 = 'How do you get the length of a string? '
-
         score = 0
         answer = 0
-        quesAndAns = [[ques1, 'def'], [ques2, 'break'], [ques3, 'len(string)']]
-        while (len(quesAndAns) > 0):
-            for q in quesAndAns:
-                random.shuffle(quesAndAns)
-                print (quesAndAns)
-                '''questionShuffled = quesAndAns
-                a = raw_input (questionShuffled[0])
-                quesAndAns.append(a)
-                questionShuffled.pop(0)'''
-        print (allArray[0][1])
+        quesAndAns = [
+            ['What is the keyword for functions in Python? ' , 'def'],
+            ['What keyword terminates a loop in Python? ', 'break'],
+            ['How do you get the length of a string? ', 'len(string)']]
+        random.shuffle(quesAndAns)
+        for qa in quesAndAns:
+            userInput = raw_input(qa[0])
+            qa.append(userInput)
+            if qa[1] == qa[2]:
+                score += 1
+        #print("Total: " + parse(score) + "/3")
+        print(score)
+        print((score//3))
     questions()
