969fd53401b569340d6f1436f55894c268e4a722
Rich Matthews <richmatthews@notonthehighstreet.com>
2016-05-13 11:30:20 +0100

    WIP

--- main_back_up.py
+++ main_back_up.py
@@ -4,73 +4,76 @@ print('Exam started')
 
 class shufflingQuestions:
     def questions():
-        overall_score = 0
-        dummy_score = 0
-        python_score = 0
-        java_score = 0
-        answer = 0
+        scores = {'overall_score' : 0, 'python' : 0, 'java' : 0, 'dummy' : 0}
+        category = {
+            'dummy': [
+                ['What is the first letter of the alphabet? ', 'a', 'dummy', ['a', 'x', 'y', 'z']],
+                ['What is the second letter of the alphabet? ', 'b', 'dummy', ['b', 's', 'm', 'v']],
+                ['What is the third letter of the alphabet? ', 'c', 'dummy', ['c', 'r', 'w', 'p']]
+            ],
+            'python': [
+                ['What is the keyword for functions in Python? ' , 'def', 'python', ['a', 'x', 'y', 'z']],
+                ['What keyword terminates a loop in Python? ', 'break', 'python', ['a', 'x', 'y', 'z']],
+                ['How do you get the length of a string? ', 'len(string)', 'python', ['a', 'x', 'y', 'z']]
+            ],
+            'java': [
+                ['What are Public, Private and Protected examples of in Java? ', 'access modifiers', 'java', ['a', 'x', 'y', 'z']],
+                ['What keyword is used to inherit a class? ' , 'extends', 'java', ['a', 'x', 'y', 'z']],
+                ['Which of these object stores association between keys and values? ', 'maps', 'java', ['a', 'x', 'y', 'z']]
+            ],
+            'arsenal': [
+                ['Who is the current Arsenal manager? ', 'Wenger', 'arsenal', ['wenger', 'mourinho', 'ferguson']],
+                ['Who is the current Arsenal captain? ', 'Ozil', 'arsenal', ['wenger', 'mourinho', 'ferguson']]
+            ],
+            'man utd': [
+                ['Who is the current Man Utd manager? ', 'Van Gaal', 'man utd', ['Van Gaal', 'mourinho', 'ferguson']],
+                ['Who is the current Man Utd captain? ', 'Rooney', 'man utd',['wenger', 'mourinho', 'ferguson']]
+            ]
+        }
 
-        multi_choice_dummy = ['a', 'x', 'y', 'z']
-        multi_choice_dummy_2 = ['b', 's', 'm', 'v']
-        multi_choice_dummy_3 = ['c', 'r', 'w', 'p']
-        multi_choice_python = ['func', 'def', 'funcy', 'function()']
-        multi_choice_java = ['inherits', 'extends', 'attaches', 'other']
+        question_set = category['dummy'] + category['python'] + category['java']
+        random.shuffle(question_set)
+        score, dummy, java, python = 0, 0, 0, 0
+        for q in question_set[:-5]:
+            random.shuffle(q[3])
+            print(q[3])
+            answer = input(q[0])
+            if answer == q[1]:
+                score += 1
+                for k, v in scores.items():
+                    if k == 'python':
+                        print (v)
 
-        category_dummy = [['What is the first letter of the alphabet? ', 'a', 'dummy'],
-                          ['What is the second letter of the alphabet? ', 'b', 'dummy'],
-                          ['What is the third letter of the alphabet? ', 'c', 'dummy']]
-
-        category_python = [['What is the keyword for functions in Python? ' , 'def', 'python'],
-                         ['What keyword terminates a loop in Python? ', 'break', 'python'],
-                         ['How do you get the length of a string? ', 'len(string)', 'python']]
-
-        category_java = [['What are Public, Private and Protected examples of in Java? ', 'access modifiers', 'java'],
-                         ['What keyword is used to inherit a class? ' , 'extends', 'java'],
-                         ['Which of these object stores association between keys and values? ', 'maps', 'java']]
-
-        random.shuffle(multi_choice_dummy)
-        random.shuffle(multi_choice_python)
-        random.shuffle(multi_choice_java)
-
-        random.shuffle(category_dummy)
-        random.shuffle(category_python)
-        random.shuffle(category_java)
-
-        possible_choices = []
-        question_set = []
-
-        possible_choices.append(multi_choice_dummy)
-        possible_choices.append(multi_choice_dummy_2)
-        possible_choices.append(multi_choice_dummy_3)
-        possible_choices.append(multi_choice_python)
-        possible_choices.append(multi_choice_java)
-
-        question_set.append(category_dummy[0])
-        question_set.append(category_dummy[1])
-        question_set.append(category_python[0])
-        question_set.append(category_python[1])
-        question_set.append(category_java[0])
-        question_set.append(category_java[1])
+                # if q[2] == ['java']:
+                #     java += 1
+                # elif q[2] == 'dummy':
+                #     dummy += 1
+                # elif q[2] == 'python':
+                #     python += 1
+        print('Score: ', str(score))
+        print('Java score: ', str(java))
+        print('Python score: ', str(python))
+        print('Dummy score: ', str(dummy))
 
 
 
-        for q_a in question_set:
-            print(possible_choices[0])
-            userInput = raw_input(q_a[0])
-            possible_choices.pop(0)
-            q_a.append(userInput)
-            if q_a[1] == q_a[3]:
-                overall_score += 1
-                if q_a[2] == 'dummy':
-                    dummy_score += 1
-                elif q_a[2] == 'python':
-                    python_score += 1
-                else:
-                    java_score += 1
-        print("You scored " + str(overall_score))
-        print("Dummy score: " + str(dummy_score))
-        print("Python score: " + str(python_score))
-        print("Java score:  " + str(java_score))
-        newscore = round((overall_score / float(len(question_set))*100),1)
-        print (str(newscore)+ "%")
+    #     for q_a in question_set:
+    #         print(possible_choices[0])
+    #         userInput = input(q_a[0])
+    #         possible_choices.pop(0)
+    #         q_a.append(userInput)
+    #         if q_a[1] == q_a[3]:
+    #             overall_score += 1
+    #             if q_a[2] == 'dummy':
+    #                 dummy_score += 1
+    #             elif q_a[2] == 'python':
+    #                 python_score += 1
+    #             else:
+    #                 java_score += 1
+    #     print("You scored " + str(overall_score))
+    #     print("Dummy score: " + str(dummy_score))
+    #     print("Python score: " + str(python_score))
+    #     print("Java score:  " + str(java_score))
+    #     newscore = round((overall_score / float(len(question_set))*100),1)
+    #     print (str(newscore)+ "%")
     questions()
